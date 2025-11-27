b2be773287d12efbd469de11a27668f236e6d619
vin <vin@vin.vin.org>
2025-11-27 10:54:44 +0200

    refact: main page

--- client/src/components/Utils/data.js
+++ client/src/components/Utils/data.js
@@ -3,5 +3,5 @@ export  const items_tiles = [
         { id: 2, name: 'Skills', description: 'Technical and soft skills I have developed.' },
         { id: 3, name: 'WorkingExperience', description: 'Professional work roles and responsibilities.' },
         { id: 4, name: 'Hackathons', description: 'Competitions and hackathon achievements.' },
-        { id: 5, name: 'Interests', description: 'Topics and activities I enjoy exploring.' }
+        //{ id: 5, name: 'Interests', description: 'Topics and activities I enjoy exploring.' }
 ];
