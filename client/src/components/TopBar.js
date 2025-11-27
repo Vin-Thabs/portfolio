02affa515e745db9dec090c485546c98c7d570b6
vin <vin@vin.vin.org>
2025-11-27 11:26:27 +0200

    style: the name

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,18 +1,25 @@
 import menuIcon from '../icons/menu.svg';
-// import profile from '../icons/profile-circle.svg';
 import '../styles/TopBar.css';
 import { useNavigate } from "react-router-dom";
+
 export default function TopBar() {
     const navigate = useNavigate();
-    function handleClick()
-    {
+
+    function handleClick() {
         navigate('/aboutme');
     }
+
     return (
         <div className="TopBar-container">
-            <img alt="menu" src={menuIcon} className="menu-icon" title='About Me' onClick={()=> handleClick()} />
-            <h1 className='dashboard-title'>Dashboard</h1>
-            {/* <img alt='profile' src={profile} className='profile-icon' title="About Me" /> */}
+            <img 
+                alt="menu"
+                src={menuIcon}
+                className="menu-icon"
+                title="About Me"
+                onClick={handleClick}
+            />
+
+            <p className='dev-ingo'>Thabiso Mahlaola</p>
         </div>
     );
 }
