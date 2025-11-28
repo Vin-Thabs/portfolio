d4de072811e224d73eb09d6fc76165ecb5dfb5b7
vin <vin@vin.vin.org>
2025-11-28 14:18:16 +0200

    feat:projects

--- client/src/components/content/Projects.js
+++ client/src/components/content/Projects.js
@@ -12,26 +12,26 @@ export default function Projects() {
             short_description: 'AI agent to play soccer against another AI agent/itself',
             long_description: 'The RoboCup 3D Soccer Simulation League, using Hungarian algorithm to find the best position for players, A* for optimal paths to goal',
             tech_stack: ['python', 'C/C++', 'bash'],
-            demo_video: 'https://www.youtube.com/watch?v=fvkqMN-t1l4',
+            demo_video: 'https://www.youtube.com/watch?v=fvkqMN-t1l4', //change the link
             github: 'https://github.com/Vin-Thabs/RoboCup-AI.git'
         },
         {
             id: 2,
             name: 'University Research Collaboration Platform',
-            short_description: 'A platform for researchers to find others to collaborate with',
-            long_description: 'This project, helps researchers to find other researchers to collaborate with, also find research to donate funds to. Export data about research topic',
+            short_description: 'A platform for researchers to find others to collaborate with.',
+            long_description: 'This project, helps researchers to find other researchers to collaborate with, also find research to donate funds to. Export data about research topic.',
             tech_stack: ['Javascript', 'ReactJs', 'MySQL', 'Azure', 'CI/CD pipeline (GitHub Actions)', 'TDD(Test Driven Development)'],
-            demo_video: 'https://www.youtube.com/watch?v=fvkqMN-t1l4',
+            demo_video: 'https://www.youtube.com/watch?v=fvkqMN-t1l4', //change the link
             github: 'https://github.com/Vin-Thabs/Uni-Research-Collab-Platform.git'
         },
         {
             id: 3,
-            name: 'Machine Learning Model',
-            short_description: 'A machine learning model to process data and identify patterns',
-            long_description: 'A predictive machine learning model built with Python and Sci-Kit Learn. It includes preprocessing pipelines, feature engineering, and hyperparameter tuning to identify hidden data patterns.',
-            tech_stack: ['Python'],
-            demo_video: 'https://www.youtube.com/watch?v=fvkqMN-t1l4',
-            github: 'https://github.com/username/ml-model'
+            name: 'FndFunder',
+            short_description: 'Funding Management is Software solution used to management funds for funding',
+            long_description: 'This project is a funding management, for funding for users. Users can donate funds, advertise the program that needs funds, upload PDFs with more information about the project that needs funding ',
+            tech_stack: ['ReactJS', 'Azure Functions', 'GitHub Actions (CI/CD)', 'Azure', 'Firebase', 'MySQL'],
+            demo_video: 'https://www.youtube.com/watch?v=1dVmYSSY_NU&feature=youtu.be',
+            github: 'https://github.com/Vin-Thabs/funding-management.git'
         }
     ];
 
